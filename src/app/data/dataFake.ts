71ef522d10c80c5ae9ec7d6bf5368ec2ffae1065
joao diego <jdflorencio@gmail.com>
2024-01-25 11:48:00 -0300

    first

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,29 @@
+export const dataFake = [
+  {
+    id: 1,
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsSbSdiQQiconD8yoHZE8B64qWx_fdS1W5lIcFN6EX5n9eD546IGwf6-r_KGy5Io3frZA&usqp=CAU",
+    title: "The Marvels",
+    description: "The Marvels é um filme de super-herói baseado nos personagens da Marvel Comics, Carol Danvers / Capitã Marvel, Kamala Khan / Ms. Marvel e Monica Rambeau / Photon. É uma sequência de Capitã Marvel (2019) e Ms. Marvel (2022), e é o 32º filme do Universo Cinematográfico Marvel (UCM). O filme é dirigido por Nia DaCosta e escrito por Megan McDonnell, e estrela Brie Larson, Iman Vellani e Teyonah Parris. No filme, Danvers, Khan e Rambeau unem forças para enfrentar uma ameaça alienígena.",
+    link: "https://www.adorocinema.com/filmes/the-marvels-2023-9429008/"
+  },
+  {
+    id: 2,
+    photo: "https://s2-g1.glbimg.com/Cqs8lrvveovF_YtQzjlU256faf8=/0x0:8256x5504/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/S/K/AD2a8eSLux9ZRg9WeABQ/homem-formiga-e-a-vespa-quantumania-paul-rudd-jonathan-majors.jpg",
+    title: "Ant-Man and the Wasp: Quantumania",
+    description: "Ant-Man and the Wasp: Quantumania é um filme de super-herói baseado nos personagens da Marvel Comics, Scott Lang / Homem-Formiga, Hope Van Dyne / Vespa e Janet Van Dyne. É a sequência de Ant-Man (2015) e Ant-Man and the Wasp (2018), e é o 33º filme do Universo Cinematográfico Marvel (UCM). O filme é dirigido por Peyton Reed e escrito por Jeff Loveness, e estrela Paul Rudd, Evangeline Lilly, Michelle Pfeiffer e Jonathan Majors. No filme, Lang, Van Dyne e Van Dyne viajam para o Reino Quântico para enfrentar Kang, o Conquistador.",
+    link: "https://www.adorocinema.com/filmes/ant-man-and-the-wasp-quantumania-2023-7439998/"
+  },
+  {
+    id: 3,
+    photo: "https://m.media-amazon.com/images/M/MV5BMjU3NGQwY2ItMDNkMi00MDk0LTgwYzAtNTBhYjFkNmYxMjQ1XkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_.jpg",
+    title: "Guardians of the Galaxy Vol. 3",
+    description: "Guardians of the Galaxy Vol. 3 é um filme de super-herói baseado nos personagens da Marvel Comics, Peter Quill / Senhor das Estrelas, Gamora, Drax, Rocket Raccoon, Groot e Mantis. É a sequência de Guardians of the Galaxy (2014) e Guardians of the Galaxy Vol. 2 (2017), e é o 34º filme do Universo Cinematográfico Marvel (UCM). O filme é dirigido por James Gunn e escrito por Gunn e Chris McKay, e estrela Chris Pratt, Zoe Saldana, Dave Bautista, Vin Diesel, Bradley Cooper e Karen Gillan. No filme, os Guardiões da Galáxia enfrentam Adam Warlock, um ser artificial criado por Ayesha para acabar com eles.",
+    link: "https://www.adorocinema.com/filmes/guardians-of-the-galaxy-vol-3-2023-11795280/"
+  },
+  {
+    id: 4,
+    photo: "https://img.olhardigital.com.br/wp-content/uploads/2022/09/blade.jpg",
+    title: "Blade",
+    description: "Blade é um filme de super-herói baseado no personagem da Marvel Comics, Blade, o Caçador de Vampiros. É um reboot da franquia Blade, e é o 35º filme do Universo Cinematográfico Marvel (UCM). O filme é dirigido por Bassam Tariq e escrito por Stacy Osei-Kuffour, e estrela Mahershala Ali como Blade."
+  }
+]
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { dataFake } from '../../data/dataFake';
+
+interface Article {
+  id: number;
+  photo: string;
+  title: string;
+  description: string;
+  link?: string;  
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  
+  public mainArticle: Article = dataFake[0];
+  public articles: Article[] = [];
+  
+  constructor() {
+    console.log(this.mainArticle)
   }
 
+  ngOnInit(): void {
+    this.articles = dataFake.filter((_, i) => i > 0 );
+  }
 }
+
+
